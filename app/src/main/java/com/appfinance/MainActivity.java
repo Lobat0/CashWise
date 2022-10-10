1fa3be5b44356a37223b6c1ad1b4543c5c2acc32
Filipe Lobato <lobato.filipe97@gmail.com>
2022-10-10 19:05:16 -0300

    Commit test

--- app/src/main/java/com/appfinance/MainActivity.java
+++ app/src/main/java/com/appfinance/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //declarar atributos
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
